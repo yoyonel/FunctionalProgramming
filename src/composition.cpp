b96f36c9bf8044e0d22d097d12df041214e3ce31
ATTY Lionel <yoyonel@hotmail.com>
2018-11-04 20:15:24 +0100

    add: composition + remove CMakeLists.txt.user from versionning

---
+++ src/composition.cpp
@@ -0,0 +1,41 @@
+#include <fplus/fplus.hpp>
+
+const auto str_to_int = fplus::read_value_unsafe<int>;
+
+int square_int(int x)
+{
+    return x*x;
+}
+
+int main(int, char *[])
+{
+    using namespace fplus;
+
+    std::vector<std::string> inputs = {"2", "3", "4"};
+
+    // 2 loops (transform) for str parsing to int and compute squaring integer
+    const auto result = fwd::apply(
+                inputs,
+                fwd::transform(str_to_int),
+                fwd::transform(square_int),
+                fwd::sum());
+    assert(result == 2*2+3*3+4*4);
+    std::cout << "sum(square(input = " << show(inputs) << ") = " << show(result) << std::endl;
+
+    // Composition can help to mutualize loops
+    const auto parse_and_square =
+            fwd::compose(str_to_int, square_int);
+    const auto result_with_composition =
+            fwd::apply(
+                inputs,
+                fwd::transform(parse_and_square),
+                fwd::sum());
+    assert(result_with_composition == result);
+    std::cout << "with composition - sum(square(input = "
+              << show(inputs) << ") = "
+              << show(result_with_composition)
+              << std::endl;
+
+    return 0;
+}
+
---
+++ src/function_composition_exercice.cpp
@@ -0,0 +1,32 @@
+#include <fplus/fplus.hpp>
+
+double str_to_double(const std::string& str)
+{
+    double result;
+    std::istringstream(str) >> result;
+    return result;
+}
+
+int main()
+{
+    using namespace fplus;
+
+    const std::string input = "1,5,4,7,2,2,3.34";
+
+    const auto result = fplus::fwd::apply(input
+        , fplus::fwd::split(',', false)
+        , fplus::fwd::transform(str_to_double)
+        , fplus::fwd::product()
+    );
+
+    std::cout << result << std::endl;
+
+    const auto parse_and_product = fplus::fwd::compose(
+           fplus::fwd::split(',', false),
+           fplus::fwd::transform(str_to_double),
+           fplus::fwd::product());
+    const auto result_with_composition = parse_and_product(input);
+    assert(result_with_composition == result);
+
+    std::cout << result_with_composition << std::endl;
+}
