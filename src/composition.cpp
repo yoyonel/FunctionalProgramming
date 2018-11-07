75a643df4b2c4e632bef698c77db3ccdaa51eabb
latty <yoyonel@hotmail.com>
2018-11-07 11:17:20 +0100

    Refacto: Format source code

--- src/composition.cpp
+++ src/composition.cpp
@@ -4,7 +4,7 @@ const auto str_to_int = fplus::read_value_unsafe<int>;
 
 int square_int(int x)
 {
-    return x*x;
+    return x * x;
 }
 
 int main(int, char *[])
@@ -15,21 +15,21 @@ int main(int, char *[])
 
     // 2 loops (transform) for str parsing to int and compute squaring integer
     const auto result = fwd::apply(
-                inputs,
-                fwd::transform(str_to_int),
-                fwd::transform(square_int),
-                fwd::sum());
-    assert(result == 2*2+3*3+4*4);
+        inputs,
+        fwd::transform(str_to_int),
+        fwd::transform(square_int),
+        fwd::sum());
+    assert(result == 2 * 2 + 3 * 3 + 4 * 4);
     std::cout << "sum(square(input = " << show(inputs) << ") = " << show(result) << std::endl;
 
     // Composition can help to mutualize loops
     const auto parse_and_square =
-            fwd::compose(str_to_int, square_int);
+        fwd::compose(str_to_int, square_int);
     const auto result_with_composition =
-            fwd::apply(
-                inputs,
-                fwd::transform(parse_and_square),
-                fwd::sum());
+        fwd::apply(
+            inputs,
+            fwd::transform(parse_and_square),
+            fwd::sum());
     assert(result_with_composition == result);
     std::cout << "with composition - sum(square(input = "
               << show(inputs) << ") = "
@@ -38,4 +38,3 @@ int main(int, char *[])
 
     return 0;
 }
-
